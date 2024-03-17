8a973b9c0cad74e0418bd47cf17d2f31e8ab6c24
runninglol <alloy1124@yahoo.com>
2024-03-17 17:33:46 -0400

    Update py files

--- USASN.py
+++ USASN.py
@@ -7,16 +7,17 @@ url = 'https://bgp.he.net/country/US'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# Extract content from tbody table where td[2] is not empty and replace "AS" with "IP-ASN,"
+# Extract content from tbody table where td[2] is not empty or td[3] is not 0
+# Replace "AS" with "IP-ASN,"
 table = soup.find('tbody')
 selected_data = []
 for row in table.find_all('tr'):
     columns = row.find_all('td')
-    if len(columns) > 1 and columns[1].text.strip():
+    if len(columns) > 2 and (columns[1].text.strip() or columns[2].text.strip() != '0'):
         selected_data.append(columns[0].text.strip().replace("AS", "IP-ASN,"))
 
 # Write the scraped content to the file with timestamp at the beginning
-timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+timestamp = datetime.now().strftime('%H:%M:%S %m/%d/%Y')
 
 with open('USASN.list', 'w') as file:
     file.write("# Generated from https://bgp.he.net/country/US at EST " + timestamp + "\n")
